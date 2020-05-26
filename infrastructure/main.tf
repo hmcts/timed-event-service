015c806e3d7dd4d1d3d75547f97c8186606c41a4
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-05-26 10:41:08 +0100

    RIA-2689 database settings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,21 @@
 provider "azurerm" {
   features {}
 }
+
+module "ia_timed_event_service_database" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = "${var.product}-${var.component}-postgres-db"
+  location           = "${var.location}"
+  env                = "${var.env}"
+  database_name      = "${var.postgresql_database_name}"
+  postgresql_user    = "${var.postgresql_user}"
+  postgresql_version = "10"
+  common_tags        = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+  subscription       = "${var.subscription}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = "${module.ia_timed_event_service_database.postgresql_password}"
+  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+}
