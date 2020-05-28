bc1659823a4a6d741cff306b5097335169e91662
makowalski <marcin.kowalski.it.consulting@gmail.com>
2020-05-28 09:23:35 +0100

    RIA-2689 terraform script (#14)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+
+  preview_vault_name           = "${var.raw_product}-aat"
+  non_preview_vault_name       = "${var.raw_product}-${var.env}"
+  key_vault_name               = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
+
+}
+
+data "azurerm_key_vault" "ia_key_vault" {
+  name                = local.key_vault_name
+  resource_group_name = local.key_vault_name
+}
+
 module "ia_timed_event_service_database" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product            = "${var.product}-${var.component}-postgres-db"
